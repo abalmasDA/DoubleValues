23bb33894e0f6ed33fe43e58d76c329772b1bbb6
abalmasDA <abalmasit@gmail.com>
2023-07-09 18:31:55 +0300

    Commit1

--- src/DoubleValues.java
+++ src/DoubleValues.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class DoubleValues {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoubleValues {
+    public static List<String> doubleValues(List<String> somelistToDoubleValues) {
+        List<String> resultList = new ArrayList<>();
+        int x = 0;
+        int y = 1;
+        for (int i = 0; i < somelistToDoubleValues.size(); i++) {
+            resultList.add(x, somelistToDoubleValues.get(i));
+            resultList.add(y, somelistToDoubleValues.get(i));
+            x += 2;
+            y += 2;
+
+        }
+        return resultList;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Використовуючи колекцію, підвійте слово:
+ * 1. Введіть із клавіатури 5 слів до списку рядків.
+ * 2. Метод doubleValues повинен підвоювати слова за принципом a, b, c -> a, a, b, b, c, c.
+ * 3. Використовуючи цикл for виведіть результат на екран, кожне значення нового рядка.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> someList = new ArrayList<>();
+        int numberWords = 5;
+
+        for (int i = 0; i < numberWords; i++) {
+            someList.add(scanner.next());
+
+        }
+        System.out.println(someList);
+
+        List<String> listDouble = DoubleValues.doubleValues(someList);
+        System.out.print("[");
+        for (int i = 0; i < listDouble.size(); i++) {
+            if (i == listDouble.size() - 1) {
+                System.out.print(listDouble.get(i) + "]");
+            } else {
+                System.out.print(listDouble.get(i) + ", ");
+            }
+
+
+        }
+
+    }
+
+
 }
